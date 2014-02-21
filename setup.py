ae4aeb5eeb778987f1b72badb67e687ddb4dd6d9
Travis Cunningham <travcunn@umail.iu.edu>
2014-02-21 15:44:31 -0500

    Added a command line utility

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ from setuptools import setup, find_packages
 setup  (
     name        = 'doublemap',
     version     = '0.0.1',
-    description = 'An API client for DoubleMap with useful route tracking utilities',
+    description = 'An API client for DoubleMap.',
     author = 'Travis Cunningham',
     author_email = 'travcunn@umail.iu.edu',
     url = 'https://github.com/travcunn/doublemap',
     license = 'MIT',
     packages  =  find_packages('src'),
-    package_dir = {'' : 'src'}
+    package_dir = {'' : 'src'},
+    entry_points = {
+        'console_scripts': [
+            'doublemap = doublemap.__init__:main',
+        ],
+    },
 )
